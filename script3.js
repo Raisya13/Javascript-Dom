6ba4b3014227284c6506c7aa69012a9e92ac72ae
Raisya13 <anastiraisyailmi13@gmail.com>
2026-01-16 22:11:27 +0700

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,24 @@
+// DOM Selection
+// document.getElementById() -> element
+const judul = document.getElementById('judul');
+judul.style.color = 'white';
+judul.style.backgroundColor = '#2770df';
+judul.innerHTML = 'Assalamualaikum';
+
+// document.getElementsByTagname()
+// -> HTMCollections
+const p = document.getElementsByTagName('p');
+
+for( let i = 0; i < p.length; i++ ) {
+
+p[i].style.backgroundColor = 'lightblue';
+}
+
+const h1 = document.getElementsByTagName('h1')[0];
+h1.style.fontSize = '50px';
+
+
+// document.getElementsByClassName()
+// -> HTMLCollection
+const p1 = document.getElementsByClassName('P1');
+P1.innerHTML = 'Ini diubah dari JavaScript';
